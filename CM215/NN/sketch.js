5fa31fe788c12503a31ec2b223b0cda318a89b05
najwamarradi <marradi46@gmail.com>
2016-10-05 09:24:54 -0500

    k

--- CM215/NN/sketch.js
+++ CM215/NN/sketch.js
@@ -8,6 +8,6 @@ function draw() {
 function touchStarted(){
     background(255);
     textSize(50);
-    text(touches[0].x,50,50);
-    text(touches[0].y,50,100);
+    rect(touches[0].x,touches[0].y,150,150);
+  
 }
