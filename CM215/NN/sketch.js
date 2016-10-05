68a4df498115931e40a5af30a7d3bb9087b57d4d
najwamarradi <marradi46@gmail.com>
2016-10-05 09:41:50 -0500

    d

--- CM215/NN/sketch.js
+++ CM215/NN/sketch.js
@@ -1,7 +1,8 @@
 function setup() {
   createCanvas(windowWidth,windowHeight);
   rectMode(CENTER);
-  
+  stroke(0);
+  strokeWeight(5);
 }
 
 function draw() {
@@ -9,7 +10,13 @@ function draw() {
 }
 function touchStarted(){
     background(255);
-    fill(random(0,255),random(0,255),random(0,255));
-    rect(touches[0].x,touches[0].y,150,150);
+    
+    for(var i=0<touches.lenght;i++){
+       rect(touches[0].x,touches[0].y,150,150);
+    }
+    
+    
+    
+    
   
 }
