9bce5ca556c5914dbd2e78f972b937014dc59783
najwamarradi <marradi46@gmail.com>
2016-10-19 09:50:17 -0500

    x

--- CM215/NN/sketch.js
+++ CM215/NN/sketch.js
@@ -1,45 +1,31 @@
-
-document.ontouchmove = function(event){
+document.ontouchmove = function(event) {
   event.preventDefault();
 }
 
 
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   stroke(0);
-  fill(red);
+  noFill();
   strokeWeight(5);
 }
 
-function draw()
-touches[(x,y),(x1,y1),(x2,y2)];
-vertex(x,y);
-vertex(x1,y1);
-vertex(x2,y2);
-fill(x)
-{
+function draw() {
   background(255);
-    
-    for(var i=0;i<touches.length;i++){
-    if((i+1)<touches.length){
-      line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
-    }
-    else{
-    line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
-    
-    }
-    
-       rect(touches[i].x,touches[i].y,150,150);
-      
-    }
-function touchStarted(){
-    
-    }
-    
-    
-    
-    
-  
+
+  for (var i = 0; i < touches.length; i++) {
+    ellipse();
+
+  }
+  fill(255, 245);
+
+  for (var i = 0; i < touches.length; i++) {
+    beginShape();
+    vertex();
+
+  }
+  endShape();
+
 }
