778507636eba7b46aa1eabda0fb0c83b0a9ed633
najwamarradi <marradi46@gmail.com>
2016-10-26 14:20:27 -0500

    a

--- CM215/rotation/1991/Untitled/sketch.js
+++ CM215/rotation/1991/Untitled/sketch.js
@@ -1,31 +1,28 @@
 var ballSize;
-var AX,VX,PX,F;
+var AX, VX, PX, F;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
   ellipseMode(CENTER);
   ballSize = 20;
 
-
-PX = windowWidth/2;
-VX=0;
-F=0,01;
+  PX = windowWidth / 2;
+  VX = 0;
+  F = 0, 01;
 }
 
 function draw() {
   background(255);
 
   drawBall();
-    textSize(45);
+  textSize(45);
 
-    text("Rx: " + floor(rotationX), 100, 100);
-    text("Ry: " + floor(rotationY), 100, 150);
-    text("Rz: " + floor(rotationZ), 100, 200);
-  }
+  text("Rx: " + floor(rotationX), 100, 100);
+  text("Ry: " + floor(rotationY), 100, 150);
+  text("Rz: " + floor(rotationZ), 100, 200);
+}
 
-  function drawBall() {
-    fill(100);
-    ellipse(windowWidth / 2, rotationX + windowHeight / 2, ballSize, ballSize);
-  }
-
-  
+function drawBall() {
+  fill(100);
+  ellipse(PX, windowHeight / 2, ballSize, ballSize);
+}
